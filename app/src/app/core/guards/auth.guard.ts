52b130f4103a24aaa2f61bf363fa17ae5040e8db
Vittorio Adesso <adessovittorio8@gmail.com>
2022-06-28 09:27:28 +0200

    config guard

--- app/src/app/core/guards/auth.guard.ts
+++ app/src/app/core/guards/auth.guard.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) { /*empty*/ }
+
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isLoggedIn !== true) {
+			//Si no hay usuario logueado no tendrá acceso
+      window.alert("Access not allowed!");
+      this.router.navigate(['singin'])
+    }
     return true;
   }
   
