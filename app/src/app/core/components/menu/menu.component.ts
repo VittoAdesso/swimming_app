2531abf033f15e16d4a8b6553378e563f152ffbf
Vittorio Adesso <adessovittorio8@gmail.com>
2022-06-30 22:25:14 +0200

    adding assets & new features

--- app/src/app/core/components/menu/menu.component.ts
+++ app/src/app/core/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public authService: AuthService) { }  
+  
   ngOnInit(): void {
+    // throw new Error('Method not implemented.');
   }
 
+  public logout() {
+    this.authService.doLogout();
+  }
 }
